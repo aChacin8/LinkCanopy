0ccec5f7aa3aff7e3b7cdcd97da54ecfc9fefef7
mrMcAko <alechacin08@gmail.com>
2025-07-22 11:38:50 -0600

    feat: Add the function createUser to the routes

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
+import { createUser } from "./handler";
 
 const router = Router()
 
-router.post('/auth/register', (req, res)=> {
-    
-})
+router.post('/auth/register',createUser)
 export default router
