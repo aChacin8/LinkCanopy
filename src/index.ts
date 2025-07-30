a9dbc4362120c1578c8a467d00bc04f5ff6cfd67
mrMcAko <alechacin08@gmail.com>
2025-07-29 20:17:19 -0600

    fix: Improved file structure.

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import colors from 'colors'
-import app from "./server";
-
-const port = process.env.PORT || 3000
-
-app.listen(port, ()=> {
-    console.log(colors.cyan.bold(`Server is running in: http://localhost:${port}/`));
-})
--- src/router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createUser } from "./handler";
-
-const router = Router()
-
-router.post('/auth/register',createUser)
-export default router
